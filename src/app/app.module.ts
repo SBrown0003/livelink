3350b326706ae8f265ec860238d642a632ed000a
Stephen Brown <all_white_forces@yahoo.com>
2019-06-11 22:52:06 -0400

    switch to use angular version of datatable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { DataTablesModule } from 'angular-datatables';
 
 import { AppComponent } from './app.component';
 
@@ -8,9 +10,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+
+    DataTablesModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
